66026c59d3985bc903aefc850826ee1c4d651bed
reed123456 <3034903604@qq.com>
2020-03-23 21:08:10 +0800

    test 2020-03-23 21:08:01

--- Test_3_22/Test4/test4.c
+++ Test_3_22/Test4/test4.c
@@ -7,48 +7,62 @@
 void menu()
 {
 	printf("*********************\n");
+	printf("**     猜 数 字    **\n");
 	printf("** 1.play   0.exit **\n");
 	printf("*********************\n");
 }
 void game()
 {
 	int guess = 0;
+	int count = 4;
 	int ret = rand() % 100 + 1;
-	//printf("%d\n", ret);
-	while (1)
+	while ( count > 0)
 	{
-		printf("猜数字：");
+		printf("\n请输入数字：>");
 		scanf("%d", &guess);
 		if (guess > ret)
 		{
-			printf("猜大了.\n");
+			if (count > 1)
+				printf("猜大了,你还有%d次机会!\n", count);
+			count--;
 		}
 		else if (guess < ret)
 		{
-			printf("猜小了.\n");
+			if (count > 1)
+				printf("猜小了,你还有%d次机会!\n", count-1);
+			count--;
 		}
-		else
+		else 
 		{
-			printf("恭喜你，猜对了！\n");
+			printf("恭喜你，猜对了!\n");
 			Sleep(1000);
 			break;
 		}
 	}
+	
+	if (count == 0)
+	{
+		printf("boom! boom! boom! you're dead!\n");
+	}
+	system("pause");
 }
+
 int main()
 {
 	int input;
 	srand((unsigned int)time(NULL));//只调用一次
 	do
 	{
+		system("cls");
 		menu();
 		printf("请选择:>");
 		scanf("%d", &input);
 		switch (input)
 		{
 		case 1:
-			game();
 			system("cls");
+			printf("这里里有一个1-100之间的数字，请你来猜一猜，你只有4次机会。\n");
+			game();
 			break;
 		case 0:
 			printf("游戏退出\n");
--- Test_3_22/Test_3_22/test1.c
+++ Test_3_22/Test_3_22/test1.c
@@ -4,21 +4,7 @@
 int main()
 {
 	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };//有序数组
-	/*int k = 7;
-	int i;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	for (i = 0; i < sz; i++)
-	{
-		if (arr[i] == k)
-		{
-			printf("找到了，下标是%d\n", i);
-			break;
-		}
-	}
-	if (i == sz)
-	{
-		printf("找不到\n");
-	}*/
+	
 	int k = 7;
 	int sz = sizeof(arr) / sizeof(arr[0]);
 	int left = 0;
