0efe81728964dda9ff76bc7ce81f7aea8f0b4b65
reed123456 <3034903604@qq.com>
2020-05-07 20:31:41 +0800

    2020-05-07 20:31:36

---
+++ Test_05_06/Test_05_06/test1.c
@@ -0,0 +1,29 @@
+struct ListNode* removeElements(struct ListNode* head, int val) {
+	if (head == NULL)
+		return NULL;
+	struct ListNode* cur = head;
+	struct ListNode* prev = NULL;
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			struct ListNode* next = cur->next;
+			if (prev == NULL)
+			{
+				head = cur->next;
+			}
+			else
+			{
+				prev->next = cur->next;
+			}
+			free(cur);
+			cur = next;
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+	}
+	return head;
+}
